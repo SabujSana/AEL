e973e3e4feb8f67c5bd3f2197c50df09ce23f2a4
SABUJ <sabujsanacse@gmail.com>
2019-04-05 11:55:00 +0600

    SplashScreenActivity added

--- app/src/main/java/com/example/sabuj/ael/activities/SecondarySplashScreenActivity.java
+++ app/src/main/java/com/example/sabuj/ael/activities/SecondarySplashScreenActivity.java
@@ -1,15 +1,24 @@
 package com.example.sabuj.ael.activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
 import com.example.sabuj.ael.R;
+import com.example.sabuj.ael.utils.SharedPreferenceManager;
 
 public class SecondarySplashScreenActivity extends AppCompatActivity {
+    private SharedPreferenceManager preferenceManager;
+    private ImageView ivSplash;
+    Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_secondary_splash_screen);
+        preferenceManager = new SharedPreferenceManager(this);
+
+        
     }
 }
--- app/src/main/java/com/example/sabuj/ael/utils/SharedPreferenceManager.java
+++ app/src/main/java/com/example/sabuj/ael/utils/SharedPreferenceManager.java
@@ -1,4 +1,41 @@
 package com.example.sabuj.ael.utils;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
 public class SharedPreferenceManager {
+    SharedPreferences preferences;
+    SharedPreferences.Editor editor;
+    Context context;
+
+    int PRIVATE_MODE = 0;
+
+    private static final String PREF_NAME = "MyPreference";
+    private static final String IS_FIRST_TIME_LAUNCH = "IsFirstTimeLaunch";
+    private static final String IS_LOGGED_IN = "isLoggedIn";
+
+    public SharedPreferenceManager(Context context) {
+        this.context = context;
+        preferences = this.context.getSharedPreferences(PREF_NAME, PRIVATE_MODE);
+        editor = preferences.edit();
+    }
+
+    public void setIsFirstTimeLaunch(boolean isFirstTimeLaunch) {
+        editor.putBoolean(IS_FIRST_TIME_LAUNCH, isFirstTimeLaunch);
+        editor.commit();
+    }
+
+    public boolean isFirstTimeLaunch() {
+        return preferences.getBoolean(IS_FIRST_TIME_LAUNCH, true);
+    }
+
+    public void setIsLoggedIn(boolean isLoggedIn) {
+        editor.putBoolean(IS_LOGGED_IN, isLoggedIn);
+        editor.commit();
+    }
+
+    public boolean getIsLoggedIn() {
+        return preferences.getBoolean(IS_LOGGED_IN, false);
+    }
+
 }
